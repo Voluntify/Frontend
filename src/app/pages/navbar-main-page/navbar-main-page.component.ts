fa5bea27b7c5335c778ca6aaf1affb6ab1091aa0
Matias Hidalgo <matias.hidalgo.2022@gmail.com>
2024-11-11 23:42:01 -0500

    navbar

--- src/app/pages/navbar-main-page/navbar-main-page.component.ts
+++ src/app/pages/navbar-main-page/navbar-main-page.component.ts
@@ -3,15 +3,17 @@ import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar-main-page',
   standalone: true,
   imports: [
-    MatIconModule,
-    MatToolbarModule,
     MatButtonModule,
-    CommonModule
+    MatToolbarModule,
+    CommonModule,
+    RouterModule,
+    MatIconModule,
   ],
   templateUrl: './navbar-main-page.component.html',
   styleUrl: './navbar-main-page.component.css'
