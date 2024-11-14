6cf5e02085ec212270bc8afc0bf152dde53abf17
Matias Hidalgo <matias.hidalgo.2022@gmail.com>
2024-11-14 17:50:39 -0500

    organization main

--- src/app/pages/organization-page/organization-page.component.ts
+++ src/app/pages/organization-page/organization-page.component.ts
@@ -1,12 +1,67 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { VoluntifyService } from '../../service/voluntify.service';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { Router } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { Organizaciones } from '../../model/organizaciones';
+import { Voluntariado } from '../../model/voluntariado';
 
 @Component({
   selector: 'app-organization-page',
   standalone: true,
-  imports: [],
+  imports: [
+    MatCardModule,
+  MatButtonModule,
+  CommonModule,
+  MatIconModule,
+  FormsModule,
+  CommonModule,
+  MatFormFieldModule,
+  MatInputModule
+  ],
   templateUrl: './organization-page.component.html',
   styleUrl: './organization-page.component.css'
 })
-export class OrganizationPageComponent {
+export class OrganizationPageComponent implements OnInit{
+  token: string | null = null;
+  organizaciones: Organizaciones[] = [];
+  voluntariados: Voluntariado[] = [];
 
+  constructor(
+    private voluntifyService: VoluntifyService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    //se obtienen la informacion de la organizacion
+    this.obtenerInfoOrganizationMain();
+    this.obtenerVoluntariadosPorOrganizacion();
+  }
+
+  obtenerInfoOrganizationMain(){
+    this.voluntifyService.obtenerInfoOrganizationMain().subscribe(
+      (data: Organizaciones[]) => {
+        this.organizaciones = data; 
+      },
+      (error) => {
+        console.error('Error al cargar la informacion', error);
+      }
+    );
+  }
+
+  obtenerVoluntariadosPorOrganizacion(){
+    this.voluntifyService.obtenerVoluntariadosPorOrganizacion().subscribe(
+      (data: Voluntariado[]) => {
+        this.voluntariados = data; 
+      },
+      (error) => {
+        console.error('Error al cargar los voluntariados', error);
+      }
+    );
+  }
 }
--- src/app/pages/profile-organization-page/profile-organization-page.component.ts
+++ src/app/pages/profile-organization-page/profile-organization-page.component.ts
@@ -1,67 +1,12 @@
-import { Component, OnInit} from '@angular/core';
-import { VoluntifyService } from '../../service/voluntify.service';
-import { CommonModule } from '@angular/common';
-import { MatCardModule } from '@angular/material/card';
-import { MatButtonModule } from '@angular/material/button';
-import { Router } from '@angular/router';
-import { MatIconModule } from '@angular/material/icon';
-import { FormsModule } from '@angular/forms';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { Organizaciones } from '../../model/organizaciones';
-import { Voluntariado } from '../../model/voluntariado';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-profile-organization-page',
   standalone: true,
-  imports: [
-  MatCardModule,
-  MatButtonModule,
-  CommonModule,
-  MatIconModule,
-  FormsModule,
-  CommonModule,
-  MatFormFieldModule,
-  MatInputModule
-  ],
+  imports: [],
   templateUrl: './profile-organization-page.component.html',
   styleUrl: './profile-organization-page.component.css'
 })
-export class ProfileOrganizationPageComponent implements OnInit{
-  token: string | null = null;
-  organizaciones: Organizaciones[] = [];
-  voluntariados: Voluntariado[] = [];
+export class ProfileOrganizationPageComponent {
 
-  constructor(
-    private voluntifyService: VoluntifyService,
-    private router: Router
-  ) {}
-
-  ngOnInit(): void {
-    //se obtienen la informacion de la organizacion
-    this.obtenerInfoOrganizationMain();
-    this.obtenerVoluntariadosPorOrganizacion();
-  }
-
-  obtenerInfoOrganizationMain(){
-    this.voluntifyService.obtenerInfoOrganizationMain().subscribe(
-      (data: Organizaciones[]) => {
-        this.organizaciones = data; 
-      },
-      (error) => {
-        console.error('Error al cargar la informacion', error);
-      }
-    );
-  }
-
-  obtenerVoluntariadosPorOrganizacion(){
-    this.voluntifyService.obtenerVoluntariadosPorOrganizacion().subscribe(
-      (data: Voluntariado[]) => {
-        this.voluntariados = data; 
-      },
-      (error) => {
-        console.error('Error al cargar los voluntariados', error);
-      }
-    );
-  }
 }
